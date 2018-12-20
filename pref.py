9504dc5374dbf245fa039a4c08a90ff4cc8dec25
Jaime Marcos <jaimemarcos11@gmail.com>
2018-12-20 12:21:18 +0800

    change the filename of db file

--- pref.py
+++ pref.py
@@ -3,33 +3,33 @@ import sqlite3, json, random, string, os
 from pathlib import Path
 from datetime import datetime
 class preferences:
-    
+
     def __init__(self):
-        self.conn = sqlite3.connect(self.__generateConfigPath() + 'Zp0NBX6kAUahb4QJnbQy0LO.db')
+        self.conn = sqlite3.connect(self.__generateConfigPath() + 'VPNConnectGUI.db')
         self.c = self.conn.cursor()
         self.c.execute('''CREATE TABLE IF NOT EXISTS conf
              (name text, value text)''')
         self.conn.commit()
-    def deleteTempFile(self, path): 
-        if os.path.exists(path): 
-            os.remove(path) 
-        else: 
+    def deleteTempFile(self, path):
+        if os.path.exists(path):
+            os.remove(path)
+        else:
             print("The file does not exist")
 
     def generateTempFileCred(self, user, p):
         path = self.__generateFileName()
-        file = open( path ,"w" ) 
+        file = open( path ,"w" )
         file.write(user + "\n")
-        file.write(p) 
-        file.close() 
-        return path       
+        file.write(p)
+        file.close()
+        return path
 
     def insertProxy(self, ip, port, status):
-        # value must be in JSON Format       
+        # value must be in JSON Format
         value = self.__dumpJSON({'ip': ip, 'port': port, 'enabled' : status})
         res = self.__checkNameExists("proxy")
         print(res)
-        if(int(res) == 1): 
+        if(int(res) == 1):
             t = (value,"proxy")
             self.__update(t)
         elif(int(res) == 0):
@@ -46,7 +46,7 @@ class preferences:
         value = self.__dumpJSON({'dir': dir})
         res = self.__checkNameExists("dir")
         print(res)
-        if(int(res) == 1): 
+        if(int(res) == 1):
             t = (value,"dir")
             self.__update(t)
         elif(int(res) == 0):
@@ -62,7 +62,7 @@ class preferences:
         value = self.__dumpJSON({'user': user, 'pass': p})
         res = self.__checkNameExists("cred")
         print(res)
-        if(int(res) == 1): 
+        if(int(res) == 1):
             t = (value,"cred")
             self.__update(t)
         elif(int(res) == 0):
@@ -75,17 +75,17 @@ class preferences:
             self.__insert(t)
         self.conn.commit()
     def getProxy(self):
-        t = ("proxy", )   
+        t = ("proxy", )
         selectResult = self.__select(t)
         if(str(selectResult) == "None"):
-            print("None") 
+            print("None")
             return ["", "", ""]
         else:
             res = self.__loadJSON(selectResult[1])
             return [res['port'], res['ip'], res['enabled']]
     def getCred(self):
         t = ("cred", )
-        selectResult = self.__select(t) 
+        selectResult = self.__select(t)
         if(str(selectResult) == "None"):
             print("None")
             return ["", ""]
@@ -94,18 +94,18 @@ class preferences:
             return [res['user'], res['pass']]
     def getDirectory(self):
         t = ("dir", )
-        selectResult = self.__select(t)   
+        selectResult = self.__select(t)
         if(str(selectResult) == "None"):
             print("None")
             return [""]
         else:
             res = self.__loadJSON(selectResult[1])
-            return [res['dir']]    
+            return [res['dir']]
     def close(self):
         if self.conn:
             print("Closing database....")
             self.conn.close()
-     
+
     def __select(self, t):
         self.c.execute('SELECT * FROM conf WHERE name=?', t)
         return self.c.fetchone()
@@ -120,7 +120,7 @@ class preferences:
     def __insert(self, t):
         self.c.execute("INSERT INTO conf VALUES (?, ? )", t)
         print("insert")
-    
+
     def __checkNameExists(self, name):
         t = (name,)
         self.c.execute('SELECT COUNT(*) FROM conf WHERE name=?', t)
@@ -128,7 +128,7 @@ class preferences:
         res = str(r[0])
         return res
 
-    
+
     def __dumpJSON(self, data):
         return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
     def __loadJSON(self, data):
@@ -138,5 +138,4 @@ class preferences:
     def __generateFileName(self):
         config = self.__generateConfigPath()
         ran = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
-        return config + ran + str(datetime.now().date()) + "_" + str(datetime.now().time()) + ".txt" 
-    
+        return config + ran + str(datetime.now().date()) + "_" + str(datetime.now().time()) + ".txt"
